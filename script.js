53ed7dbaba063640679a1aae4180be580bce9da2
mayday9 <themaydaygmt@gmail.com>
2018-06-26 20:46:44 +0300

    loading

---
+++ script.js
@@ -0,0 +1,48 @@
+//todo planet
+
+/*var access = document.getElementById("code9");
+var code = access.innerHTML;
+code = code + " midnight";
+alert(code);*/
+
+/*var planet = document.getElementById("greenplanet");
+
+planet.innerHTML = "Red Alert: hit by phaser fire!";*/
+
+
+// todo classwork
+/*var task1 = document.getElementById("e7").innerHTML;
+var task2 = document.getElementById("e8").innerHTML;
+var task3 = document.getElementById("e16").innerHTML;
+var task4 = document.getElementById("e9").innerHTML;
+var task5 = document.getElementById("e18").innerHTML;
+var task6 = document.getElementById("e13").innerHTML;
+var task7 = document.getElementById("e12").innerHTML;
+var task8 = document.getElementById("e2").innerHTML;
+
+console.log(task1, task2, task3, task4, task5, task6, task7, task8);*/
+
+
+//todo next
+/*var tas1 = document.getElementById("e1").innerText;
+var tas2 = document.getElementById("e2").innerText;
+
+console.log(tas1, tas2);*/
+
+//todo poem
+var tas1 = document.getElementById("e1");
+var tas2 = document.getElementById("e2");
+var tas3 = document.getElementById("e3");
+
+
+
+tas1.innerHTML = document.getElementById("e3").innerText;
+tas2.innerHTML = document.getElementById("e2").innerText;
+tas3.innerHTML = document.getElementById("e1").innerText;
+
+console.log(tas1, tas2, tas3);
+
+
+
+
+
